e93653f72f3d8d4e89e2cc1406203e5b01ae650a
Bogdan Andrushchenko <bandrushchenko15@gmail.com>
2020-11-02 11:57:16 +0200

    add js logic 5 task

--- src/js/5libraryPage.js
+++ src/js/5libraryPage.js
@@ -1,38 +1,72 @@
 console.log('File 5libraryPage.js connected');
-import li_itemTPL from '../templates/mylibraryFilm.hbs';
 
-/*
- *- берем из DOM  ul в которой будет или список очереди просмотра или список просмотренных фильмов; Done
- */
-const refFilmLibraryUl = document.querySelector('.js-mylibraryFilmList');
-/**
-         * - создаем функцию createLibraryCardFunc, принимает параметрами imgPath, filmTitle, movieId, voteAverage, 
-        создает li согласно макета, вешает на нее слушателем функцию activeDetailsPage c параметрами movieId и флагом true
-         так как фильм из библиотеки (смотри пункт “3)” создание activeDetailsPage);
-         */
+import li_itemTPL from '../templates/li_templete.hbs';
+import data from './array-test.json'; // підставне, замінити або видалити
 
-const createLibraryCardFunc = (imgPath, filmTitle, movieId, voteAverage) => {
-  const markup = li_itemTPL(data);
-//   refFilmLibraryUl.insertAdjacentHTML('beforeend', markup);
+const libraryFilmListUlRef = document.querySelector('.js-watched-list-film');
 
+const createLibraryCardFunc = movieId => {
+  const filmListItem = li_itemTPL(data);
 
+  //   filmListItem.addEventListener('click', () =>
+  //     activeDetailsPage(movieId, true),
+  //   ); розкоментувати!!!
 
-return markup
+  return filmListItem;
 };
 
-/**
-         * - создаем функцию drawQueueFilmList, создает в себе переменную фрагмент, читает во вторую переменую из 
-        local storage ключ filmsQueue и если ее значение не пустое и не равно нулю длина массива то проходим по нему
-         и в созданный фрагмент в цикле кладем созданную li функцией createLibraryCardFunc, потом зачищаем все в ul списке библиотеки
-        и кладем фрагмент в нее, в другом случае если в local storage пустота то и длина массива равна нулю то отрисовать заглушку 
-        “You do not have to queue movies to watch. Add them.”, и удаляет класс активной кнопки у просмотренных фильмов и добавляет такой класс 
-        кнопке очереде просомотра;
-         */
+////////мусор видалити 
+/**https://api.themoviedb.org/3/search/movie?api_key=c5a07ae819d95fe3119594f7b17c2bc2&query=Jack+Reacher */
+const li = createLibraryCardFunc();
+console.log(typeof li);
+ localStorage.setItem('filmsQueue', JSON.stringify(data)); //const testLocalStorage =
+// localStorage.clear('filmsQueue', JSON.stringify(data)); //const testLocalStorage =
+///////////////////////////////////////////////////////////
+
 
 const drawQueueFilmList = () => {
-    let filmsQueueArr = [];
-    let localStorageData = localStorage.getItem('filmsQueue');
-    if (localStorageData !== null) {
-      filmsQueueArr.push(...JSON.parse(localStorageData));
-    }
+ 
+  let listQueueFromLocalSt = localStorage.getItem('filmsQueue');
+  const arrParse = JSON.parse(listQueueFromLocalSt);
+    console.log(arrParse.results);
+  if (listQueueFromLocalSt !== null && arrParse.length !== 0) {
+    libraryFilmListUlRef.innerHTML = '';
+    libraryFilmListUlRef.insertAdjacentHTML('beforeend', createLibraryCardFunc());
+
+  }
+  if (listQueueFromLocalSt === null || arrParse.length === 0) {
+    libraryFilmListUlRef.innerHTML = '';
+    const listItemMessage = document.createElement('li');
+    listItemMessage.classList.add('класс-для-цього-месседжа');
+    listItemMessage.textContent =
+      'You do not have to queue movies to watch. Add them.';
+    libraryFilmListUlRef.append(listItemMessage);
+    console.log(listItemMessage);
+  }
+  //кнопка активна   queueListButton.classList.add('класс');
+  //кнопка не активна   watchedListButton.classList.remove('класс');
 };
+drawQueueFilmList();
+
+const drawWatchedFilmList = () => {
+ 
+    let listQueueFromLocalSt = localStorage.getItem('filmsWatched');
+    const arrParse = JSON.parse(listQueueFromLocalSt);
+      console.log(arrParse.results);
+    if (listQueueFromLocalSt !== null && arrParse.length !== 0) {
+      libraryFilmListUlRef.innerHTML = '';
+      libraryFilmListUlRef.insertAdjacentHTML('beforeend', createLibraryCardFunc());
+  
+    }
+    if (listQueueFromLocalSt === null || arrParse.length === 0) {
+      libraryFilmListUlRef.innerHTML = '';
+      const listItemMessage = document.createElement('li');
+      listItemMessage.classList.add('класс-для-цього-месседжа');
+      listItemMessage.textContent =
+        'You do not have to queue movies to watch. Add them.';
+      libraryFilmListUlRef.append(listItemMessage);
+      console.log(listItemMessage);
+    }
+    //кнопка активна   queueListButton.classList.add('класс');
+    //кнопка не активна   watchedListButton.classList.remove('класс');
+  };
