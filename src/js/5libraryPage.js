e49dacde916c71913eb5a3a8eff37c7e8786b2bf
Bogdan Andrushchenko <bandrushchenko15@gmail.com>
2020-11-02 13:26:37 +0200

    corect 5task

--- src/js/5libraryPage.js
+++ src/js/5libraryPage.js
@@ -1,13 +1,12 @@
 
-console.log('File 5libraryPage.js connected');
 
-import li_itemTPL from '../templates/li_templete.hbs';
-import data from './array-test.json'; // підставне, замінити або видалити
+import li_itemTPL from '../templates/mylibraryFilm.hbs';
+// import data from './array-test.json'; // підставне, замінити або видалити
 
 const libraryFilmListUlRef = document.querySelector('.js-watched-list-film');
 
 const createLibraryCardFunc = movieId => {
-  const filmListItem = li_itemTPL(data);
+  const filmListItem = li_itemTPL();
 
   //   filmListItem.addEventListener('click', () =>
   //     activeDetailsPage(movieId, true),
@@ -18,14 +17,14 @@ const createLibraryCardFunc = movieId => {
 
 ////////мусор видалити 
 /**https://api.themoviedb.org/3/search/movie?api_key=c5a07ae819d95fe3119594f7b17c2bc2&query=Jack+Reacher */
-const li = createLibraryCardFunc();
-console.log(typeof li);
- localStorage.setItem('filmsQueue', JSON.stringify(data)); //const testLocalStorage =
-// localStorage.clear('filmsQueue', JSON.stringify(data)); //const testLocalStorage =
+// const li = createLibraryCardFunc();
+// console.log(typeof li);
+//  localStorage.setItem('filmsQueue', JSON.stringify(data)); //const testLocalStorage =
+//  localStorage.removeItem('filmsQueue', JSON.stringify(data)); //const testLocalStorage =
 ///////////////////////////////////////////////////////////
 
 
-const drawQueueFilmList = () => {
+export const drawQueueFilmList = () => {
  
   let listQueueFromLocalSt = localStorage.getItem('filmsQueue');
   const arrParse = JSON.parse(listQueueFromLocalSt);
@@ -47,9 +46,9 @@ const drawQueueFilmList = () => {
   //кнопка активна   queueListButton.classList.add('класс');
   //кнопка не активна   watchedListButton.classList.remove('класс');
 };
-drawQueueFilmList();
+// drawQueueFilmList();
 
-const drawWatchedFilmList = () => {
+export const drawWatchedFilmList = () => {
  
     let listQueueFromLocalSt = localStorage.getItem('filmsWatched');
     const arrParse = JSON.parse(listQueueFromLocalSt);
